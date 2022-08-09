ba358219e6d0bd43f3feb93a545a08ed4a12da38
hariomr <hariomrathore19@gmail.com>
2022-08-09 09:34:22 +0530

    linked list

---
+++ Circular.cpp
@@ -0,0 +1,108 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+
+    public:
+    int data;
+    Node* next;
+
+    // constructor
+    Node(int d){
+        this -> data = d;
+        this -> next = NULL;
+    }
+
+    // destructor
+    ~Node(){
+        int value = this ->data;
+        if(next != NULL){
+            delete next;
+            next = NULL; 
+        }
+        cout << "Memory is free for node with data " << value << endl;
+    }
+
+};
+
+void insertAtNode(Node* &tail, int element , int d){
+    // assuming that the element is present in the list
+    if(tail == NULL){
+        Node* newNode = new Node(d);
+        tail = newNode;
+        newNode -> next = newNode;
+    }
+    else{
+        // non-empty list
+        Node* curr = tail;
+        
+        while(curr->data != element){
+            curr = curr -> next;
+        }
+        
+        Node* temp = new Node(d);
+        temp -> next = curr -> next;
+        curr -> next = temp;
+
+
+    }
+}
+void print (Node* tail){
+    Node* temp = tail;
+
+    if(tail == NULL){
+        cout << "List is empty" << endl;
+        return;
+    }
+    do{
+        cout << tail ->data <<" ";
+        tail = tail -> next;
+    }while(tail != temp);
+    cout << endl;
+}
+
+void deleteNode(Node* &tail, int value){
+    // empty list
+    if(tail == NULL){
+        cout << "List is empty, please check again" << endl;
+    }
+    else{
+        // non-empty
+
+        Node* prev = tail;
+        Node* curr = prev ->next;
+
+        while(curr -> data != value){
+            prev = curr;
+            curr = curr -> next;
+        }
+        prev -> next = curr -> next;
+
+        // one Node linked list 
+        if(curr == prev){
+            tail = NULL;
+        }
+        if(tail == curr){
+            tail = prev;
+        }
+        curr -> next = NULL;
+        delete curr;
+    }
+}
+int main(){
+    Node* tail = NULL;
+
+    insertAtNode(tail,3,5);
+    print(tail); 
+
+    // insertAtNode(tail,5,7);
+    // print(tail); 
+
+    // insertAtNode(tail,7,9);
+    // print(tail); 
+
+    deleteNode(tail ,5);
+    print(tail);
+
+    return 0;
+}
