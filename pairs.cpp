a2c0b19274e0a56c1eb2aca210bd069f819695ba
hariomr <hariomrathore19@gmail.com>
2022-08-23 08:04:04 +0530

    stl

---
+++ pairs.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // pair is an stl container which allow us to store pairs.
+    pair<int , char> p;
+    p.first = 3;
+    p.second = 'f';
+
+
+    return 0;
+}
